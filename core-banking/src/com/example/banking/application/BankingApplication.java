6c42d26e3c48098f5ab7f143137c6555023897fa
Binnur KURT <deepcloudlabs@gmail.com>
2024-02-26 16:13:06 +0300

    initial commit

---
+++ core-banking/src/com/example/banking/application/BankingApplication.java
@@ -0,0 +1,16 @@
+package com.example.banking.application;
+
+import com.example.banking.domain.Account;
+
+public class BankingApplication {
+	public static void main(String[] args) {
+		// uses constructor #2
+		Account acc1 = new Account("tr1", 0.0);
+		acc1.withdraw(200_000_000);
+		// Error: final -> acc1.iban = "tr4";
+		// uses constructor #1
+		Account acc2 = new Account("tr2");
+		acc2.deposit(10_000_000);
+		acc2.withdraw(200_000_000);
+	}
+}
---
+++ core-banking/src/com/example/banking/domain/Account.java
@@ -0,0 +1,38 @@
+package com.example.banking.domain;
+
+public class Account {
+	// members: attributes, methods
+	// attributes: iban, balance
+	private final String iban; // hidden?
+	private double balance; // hidden?
+	// methods: constructor, getter, withdraw, deposit
+
+	public Account(String iban) { // constructor #1
+		this.iban = iban;
+		this.balance = 0.0;
+	}
+	
+	public Account(String iban, double balance) { // constructor #2
+		this.iban = iban;
+		this.balance = balance;
+	}
+	// business method
+	public boolean deposit(double amount) {
+		// validation
+		if (amount <= 0.0) return false;
+		// business logic
+		this.balance = this.balance + amount;
+		return true;
+	}
+	
+	// business method
+	public boolean withdraw(double amount) {
+		// validation rule
+		if (amount <= 0.0) return false;
+		// business rule
+		if (amount > this.balance) return false;
+		// business logic
+		this.balance = this.balance - amount;
+		return true;
+	}
+}
