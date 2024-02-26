0d86c6925d65bc480022c1a6e0ccc61b41e5e440
Binnur KURT <deepcloudlabs@gmail.com>
2024-02-26 16:14:16 +0300

    Update BankingApplication.java

--- core-banking/src/com/example/banking/application/BankingApplication.java
+++ core-banking/src/com/example/banking/application/BankingApplication.java
@@ -7,10 +7,12 @@ public class BankingApplication {
 		// uses constructor #2
 		Account acc1 = new Account("tr1", 0.0);
 		acc1.withdraw(200_000_000);
+		System.out.println(acc1.getBalance());
 		// Error: final -> acc1.iban = "tr4";
 		// uses constructor #1
 		Account acc2 = new Account("tr2");
 		acc2.deposit(10_000_000);
 		acc2.withdraw(200_000_000);
+		System.out.println(acc2.getBalance());
 	}
 }
