599b7eaa494ebfc603890202a1372f6389faa2d0
deepcloudlabs <deepcloudlabs@gmail.com>
2024-02-27 15:25:40 +0300

    Update AccountTest.java

--- core-banking/test/com/example/banking/domain/AccountTest.java
+++ core-banking/test/com/example/banking/domain/AccountTest.java
@@ -1,8 +1,11 @@
 package com.example.banking.domain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
@@ -18,4 +21,28 @@ class AccountTest {
 		assertEquals(balance, account.getBalance());
 	}
 
+	@DisplayName("deposit with negative amount should return false")
+	@Test
+	void depositWithNegativeAmountShouldFail() {
+		// 1. Test Fixture/Setup
+		Account account = new Account("tr1", 1_000);
+		// 2. Call Exercise Method: deposit
+		boolean result = account.deposit(-1);
+		// 3. Verification
+		assertFalse(result);
+		// 4. Tear down 
+	}
+	
+	@DisplayName("deposit with positive amount should be successful")
+	@Test
+	void depositWithPositiveAmountShouldSuccess() {
+		// 1. Test Fixture/Setup
+		Account account = new Account("tr1", 1_000);
+		// 2. Call Exercise Method: deposit
+		boolean result = account.deposit(1);
+		// 3. Verification
+		assertTrue(result);
+		assertEquals(1_001, account.getBalance());
+		// 4. Tear down 
+	}
 }
