68d05ed4bd38f953000ae84c64c82222d3dc6ca7
Binnur KURT <deepcloudlabs@gmail.com>
2024-02-28 13:47:54 +0300

    Create Customer.java

---
+++ core-banking/src/com/example/banking/domain/Customer.java
@@ -0,0 +1,11 @@
+package com.example.banking.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Customer {
+	private String identity;
+	private String fullName;
+	private List<Account> accounts = new ArrayList<>();
+	
+}
