9af5610ffcd007b8963d6eb2502a2469d91225be
Binnur KURT <deepcloudlabs@gmail.com>
2024-02-28 14:09:15 +0300

    Update Customer.java

--- core-banking/src/com/example/banking/domain/Customer.java
+++ core-banking/src/com/example/banking/domain/Customer.java
@@ -2,10 +2,32 @@ package com.example.banking.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
-public class Customer {
-	private String identity;
+public final class Customer {
+	private final String identity;
 	private String fullName;
-	private List<Account> accounts = new ArrayList<>();
-	
+	private final List<Account> accounts = new ArrayList<>();
+
+	public Customer(final String identity, final String fullName) {
+		this.identity = identity;
+		this.fullName = fullName;
+	}
+
+	public String getIdentity() {
+		return this.identity;
+	}
+
+	public String getFullName() {
+		return this.fullName;
+	}
+
+	// violates information hiding principle!
+	public List<Account> getAccounts() {
+		return new ArrayList<>(accounts);
+	}
+
+	public void addAccount(Account account) {
+		this.accounts.add(account);
+	}
 }
