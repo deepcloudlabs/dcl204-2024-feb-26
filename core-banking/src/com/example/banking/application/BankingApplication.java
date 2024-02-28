0c352cd4949923b0feadeea1a2d3b3291b80b439
Binnur KURT <deepcloudlabs@gmail.com>
2024-02-28 14:09:18 +0300

    Update BankingApplication.java

--- core-banking/src/com/example/banking/application/BankingApplication.java
+++ core-banking/src/com/example/banking/application/BankingApplication.java
@@ -2,6 +2,7 @@ package com.example.banking.application;
 
 import com.example.banking.domain.Account;
 import com.example.banking.domain.CheckingAccount;
+import com.example.banking.domain.Customer;
 
 @SuppressWarnings("unused")
 public class BankingApplication {
@@ -21,5 +22,15 @@ public class BankingApplication {
 		// numbers: i) value-typed variable
 		//         ii) local variable 
 		int number = 42;
+		Customer jack = new Customer("11111111110", "Jack Bauer");
+		jack.getAccounts().add(new Account("tr5"));
+		jack.addAccount(new Account("tr6"));
+		jack.addAccount(new CheckingAccount("tr7",1,1));
+		System.out.println(jack.getAccounts());
+		jack.getAccounts().clear();
+		System.out.println(jack.getAccounts());
+		System.out.println(jack.getFullName()); 
+		System.out.println(jack.getFullName().toUpperCase());
+		System.out.println(jack.getFullName()); 
 	}
 }
