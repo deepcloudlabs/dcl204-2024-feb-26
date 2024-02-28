5fa9e359caa686772244226e37902d50fdf8ccf1
Binnur KURT <deepcloudlabs@gmail.com>
2024-02-28 07:01:10 +0300

    Update AccountTest.java

--- core-banking/test/com/example/banking/domain/AccountTest.java
+++ core-banking/test/com/example/banking/domain/AccountTest.java
@@ -14,11 +14,19 @@ class AccountTest {
 
 	@DisplayName("successfully created")
 	@ParameterizedTest
-	@CsvFileSource( resources = "accounts.csv")
-	void createAccountSuccessfully(String iban,double balance) {
+	@CsvFileSource(resources = "accounts.csv")
+	void createAccountSuccessfully(String iban, double balance) {
 		Account account = new Account(iban, balance);
 		assertEquals(iban, account.getIban());
 		assertEquals(balance, account.getBalance());
+	}
+	
+	@DisplayName("successfully created")
+	@Test
+	void createAccountWithZerBalanceSuccessfully() {
+		Account account = new Account("tr1");
+		assertEquals("tr1", account.getIban());
+		assertEquals(0.0, account.getBalance());
 	}
 
 	@DisplayName("deposit with negative amount should return false")
@@ -30,9 +38,9 @@ class AccountTest {
 		boolean result = account.deposit(-1);
 		// 3. Verification
 		assertFalse(result);
-		// 4. Tear down 
+		// 4. Tear down
 	}
-	
+
 	@DisplayName("deposit with positive amount should be successful")
 	@Test
 	void depositWithPositiveAmountShouldSuccess() {
@@ -43,6 +51,71 @@ class AccountTest {
 		// 3. Verification
 		assertTrue(result);
 		assertEquals(1_001, account.getBalance());
-		// 4. Tear down 
+		// 4. Tear down
+	}
+
+	@DisplayName("withdraw with negative amount should return false")
+	@Test
+	void withdrawWithNegativeAmountShouldFail() {
+		// 1. Test Fixture/Setup
+		Account account = new Account("tr1", 1_000);
+		// 2. Call Exercise Method: deposit
+		boolean result = account.withdraw(-1);
+		// 3. Verification
+		assertFalse(result);
+		// 4. Tear down
+	}
+
+	@DisplayName("withdraw with amount less than balance should be successful")
+	@Test
+	void withdrwWithAmountLessThanBalanceShouldSuccess() {
+		// 1. Test Fixture/Setup
+		Account account = new Account("tr1", 1_000);
+		// 2. Call Exercise Method: deposit
+		boolean result = account.withdraw(1);
+		// 3. Verification
+		assertTrue(result);
+		assertEquals(999, account.getBalance());
+		// 4. Tear down
+	}
+
+	@DisplayName("withdraw all balance should be successful")
+	@Test
+	void withdrwAllBalanceShouldSuccess() {
+		// 1. Test Fixture/Setup
+		Account account = new Account("tr1", 1_000);
+		// 2. Call Exercise Method: deposit
+		boolean result = account.withdraw(1_000);
+		// 3. Verification
+		assertTrue(result);
+		assertEquals(0, account.getBalance());
+		// 4. Tear down
+	}
+	
+	@DisplayName("withdraw more than balance should fail")
+	@Test
+	void withdrwMoreThanBalanceShouldFail() {
+		// 1. Test Fixture/Setup
+		Account account = new Account("tr1", 1_000);
+		// 2. Call Exercise Method: deposit
+		boolean result = account.withdraw(1_000.1);
+		// 3. Verification
+		assertFalse(result);
+		assertEquals(1_000, account.getBalance());
+		// 4. Tear down
+	}
+	
+	@DisplayName("toString should contain iban and balance")
+	@Test
+	void toStringShouldContainIbanAndBalance() {
+		// 1. Test Fixture/Setup
+		Account account = new Account("tr1", 1_000);
+		// 2. Call Exercise Method: deposit
+		String result = account.toString();
+		// 3. Verification
+		assertTrue(result.contains("iban"));
+		assertTrue(result.contains("balance"));
+		assertTrue(result.contains("Account"));
+		// 4. Tear down
 	}
 }
