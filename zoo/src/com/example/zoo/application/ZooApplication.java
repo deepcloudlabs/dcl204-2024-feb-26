3bb97c99288cffaf1e2baea8496c057c46cf64f9
Binnur KURT <deepcloudlabs@gmail.com>
2024-02-28 16:19:58 +0300

    Create ZooApplication.java

---
+++ zoo/src/com/example/zoo/application/ZooApplication.java
@@ -0,0 +1,29 @@
+package com.example.zoo.application;
+
+import java.util.List;
+
+import com.example.zoo.domain.Animal;
+import com.example.zoo.domain.Cat;
+import com.example.zoo.domain.Fish;
+import com.example.zoo.domain.Pet;
+import com.example.zoo.domain.Spider;
+
+public class ZooApplication {
+
+	public static void main(String[] args) {
+		List<Animal> animals = List.of(
+				new Cat("Garfield"),
+				new Cat(),
+				new Spider(),
+				new Fish("Free Willy"),
+				new Spider(),
+				new Fish("Nemo")
+		);
+		for (Animal animal : animals) {
+			animal.walk();
+			animal.eat();
+			if (animal instanceof Pet)
+			   ((Pet)animal).play();
+		}
+	}
+}
