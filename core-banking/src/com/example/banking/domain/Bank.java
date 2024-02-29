06a1996c9a89faf6041c9708b32a0b42b504ded7
Binnur KURT <deepcloudlabs@gmail.com>
2024-02-29 09:53:33 +0300

    Create Bank.java

---
+++ core-banking/src/com/example/banking/domain/Bank.java
@@ -0,0 +1,13 @@
+package com.example.banking.domain;
+
+public class Bank {
+   // bddkId: int
+   // name: String
+   // customers: List<Customer>
+   //TODO: constructor
+   //TODO: getter: bddkId, name, customers
+   //TODO: setter: name
+   //TODO: createCustomer(identity,fullname) -> Customer	
+   //TODO: findCustomerByIdentity(identity) -> Customer
+   //TODO: getTotalBalance() -> double	
+}
